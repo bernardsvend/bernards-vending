8939982bef2cad22947633fd90538eb83b34998a
bernardsvend <bernardsvending@gmail.com>
2026-08-24 17:11:12 -0500

    Route Cooler Rewards sales page

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { AiAssistant } from './components/AiAssistant';
 
 // Pages / Sections
 import { Home } from './pages/Home';
+import { CoolerRewards } from './pages/CoolerRewards';
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-slate-50">
       <Navbar />
       <main className="flex-grow">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cooler-rewards" element={<CoolerRewards />} />
+        </Routes>
       </main>
       <Footer />
       <AiAssistant />
